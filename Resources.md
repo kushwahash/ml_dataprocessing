7d1df24048da5aed69db1fe23817461942ac22b8
Shailesh Kushwaha <shaileshkushwaha@Shaileshs-MacBook-Pro.local>
2019-02-05 08:09:31 -0600

    Feature scaing. Add new file resources.

---
+++ Resources.md
@@ -0,0 +1,12 @@
+We can use below resources to understand and learn more about the differenct techniques used.
+
+A)
+To scale data, following can be used
+1. Normalization, which scales all numeric variables in the range [0,1]. One possible formula is given below:
+   Xnew = (X - Xmin)/(Xmax-Xmin)
+
+
+2.Standardization on your data set. It will then transform it to have zero mean and unit variance.
+  Xnew = (X - mean(X))/(Standard Deviation of X)
+
+Standard Deviation  = measure of spread (whether a data point is near to mean (standard) or away from the mean(deviated))
--- data_processing.py
+++ data_processing.py
@@ -43,3 +43,16 @@ Y = encode_Y.fit_transform(Y)
 #splitting the dataset for Training and Test
 from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test = train_test_split(X,Y,test_size = 0.2,random_state=0)
+
+'''
+feature scaling to avoid dominance of one feature. For example, some data mining techniques use the 
+Euclidean distance. Therefore, all parameters should have the same scale for a fair comparison between them.
+Example, salary here is big range number which will dominate age. 
+So, scale it We can use standarisation or normalisation
+'''
+
+#feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
